d7b3112bae2735b702cfb496b5b97369cf4fa0bd
Timoxa-svg <timofeevitch.mamaev@yandex.ru>
2025-04-09 20:03:46 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ const Player = dynamic(
   () => import("@lottiefiles/react-lottie-player").then((mod) => mod.Player),
   { ssr: false }
 );
-const Particles = dynamic(() => import("react-particles"), { ssr: false });
+const Particles = dynamic(() => import("@react-particles"), { ssr: false });
 
 // Настройка частиц
 const particlesInit = async (engine) => {
